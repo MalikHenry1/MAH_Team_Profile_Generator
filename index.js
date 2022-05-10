4522de91602fc5f4a0bae3b5dbdae2ac200f6e02
Malik Henry <mhenry30658@gmail.com>
2022-05-10 18:36:56 -0400

    final push, assignment comlpeted

--- index.js
+++ index.js
@@ -1,178 +1,243 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-const Manager = require('./lib/Manager');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
-const render = require('./src/page-template');
-
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const render = require("./src/page-template");
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 const teamMembers = {
   manager: null,
   engineers: [],
   interns: [],
-}
+};
+const idList = [];
 
 function createManager() {
-  inquirer.prompt([
-    {
-      type: 'input',
-      name: 'managerName', // stopped here , continue when you rewatch video.
-    }
-  ])
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "managerName",
+        validate: (answer) => {
+          if (answer !== "") {
+            return true;
+          }
+          return "You have entered an empty string, please enter at least one valid character.";
+        },
+      },
+      {
+        type: "input",
+        name: "managerId",
+        message: "Please enter the manager's ID: ",
+        validate: (answer) => {
+          const pass = answer.match(/^[1-9]\d*$/);
+          if (pass) {
+            return true;
+          }
+          return "Please enter a positive number greater than 0";
+        },
+      },
+      {
+        type: "input",
+        name: "managerEmail",
+        message: "Please enter your manager's email: ",
+        validate: (answer) => {
+          const pass = answer.match(/\S+@\S+\.\S+/);
+          if (pass) {
+            return true;
+          }
+          return "Please enter a valid email.";
+        },
+      },
+      {
+        type: "input",
+        name: "managerOfficeNumber",
+        message: "Please enter the manager's office number: ",
+        validate: (answer) => {
+          const pass = answer.match(/^[1-9]\d*$/);
+          if (pass) {
+            return true;
+          }
+          return "Please enter a positive number greater than 0";
+        },
+      },
+    ])
+    .then((answers) => {
+      const manager = new Manager(
+        answers.managerName,
+        answers.managerId,
+        answers.managerEmail,
+        answers.managerOfficeNumber
+      );
+      teamMembers.manager = manager;
+      idList.push(answers.managerId);
+      makeTeam();
+    });
 }
 
-function render(teamMembers) {
-  return 'This will be HTML'
-}
-
-function createTeam() {
-  inquirer.prompt([
-    {
-      type: 'list',
-      name: 'choice',
-      message: 'Which type of team member would you like to add?',
-      choices: [
-        'Engineer',
-        'Intern',
-        'I am finished building my team.'
-      ]
-    }
-  ]) .then((answers) => {
-    switch(answers.choice) {
-      case 'Engineer': 
-        addEngineer();
-        break;
-      case 'Intern':
+function makeTeam() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "choice",
+        message: "Which team member would you like to add next?",
+        choices: ["Engineer", "Intern", "I do not want to add any more"],
+      },
+    ])
+    .then((answers) => {
+      switch (answers.choice) {
+        case "Engineer":
+          addEngineer();
+          break;
+        case "Intern":
           addIntern();
           break;
-      default: 
-        buildTeam();
-    }
-  })
+        default:
+          buildTeam();
+      }
+    });
 }
 
 function addEngineer() {
-
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        validate: (answer) => {
+          if (answer !== "") {
+            return true;
+          }
+          return "Please enter at least one character.";
+        },
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Please enter this engineer's id: ",
+        validate: (answer) => {
+          const pass = answer.match(/^[1-9]\d*$/);
+          if (pass) {
+            if (idList.includes(answer)) {
+              return "This id is already taken, please enter a unique id.";
+            } else {
+              return true;
+            }
+          }
+          return "Please enter a positive number greater than 0.";
+        },
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Please enter this engineer's email: ",
+        validate: (answer) => {
+          const pass = answer.match(/\S+@\S+\.\S+/);
+          if (pass) {
+            return true;
+          }
+          return "Please enter a valid email.";
+        },
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "Please enter this engineer's GitHub username: ",
+        validate: (answer) => {
+          if (answer !== "") {
+            return true;
+          }
+          return "Please enter at least one character.";
+        },
+      },
+    ])
+    .then((answers) => {
+      const engineer = new Engineer(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.github
+      );
+      teamMembers.engineers.push(engineer);
+      idList.push(answers.id);
+      makeTeam();
+    });
 }
 
 function addIntern() {
-
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        validate: (answer) => {
+          if (answer !== "") {
+            return true;
+          }
+          return "Please enter at least one character.";
+        },
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Please enter this intern's ID: ",
+        validate: (answer) => {
+          const pass = answer.match(/^[1-9]\d*$/);
+          if (pass) {
+            if (idList.includes(answer)) {
+              return "This id is already taken, please enter a unique id.";
+            } else {
+              return true;
+            }
+          }
+          return "Please enter a positive number greater than 0.";
+        },
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Please enter this intern's email address: ",
+        validate: (answer) => {
+          const pass = answer.match(/\S+@\S+\.\S+/);
+          if (pass) {
+            return true;
+          }
+          return "Please enter a valid email.";
+        },
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "Please enter this intern's school: ",
+        validate: (answer) => {
+          if (answer !== "") {
+            return true;
+          }
+          return "Please enter at least one character.";
+        },
+      },
+    ])
+    .then((answers) => {
+      const intern = new Intern(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.school
+      );
+      teamMembers.interns.push(intern);
+      idList.push(answers.id);
+      makeTeam();
+    });
 }
 
 function buildTeam() {
-  fs.writeFile("/dist/team.html", render(teamMembers), (err) => {
-    if(err) {
+  fs.writeFile("dist/team.html", render(teamMembers), (err) => {
+    if (err) {
       console.log(err);
     }
-  })
+  });
+  console.log("Your team profile page has been successfully generated!");
 }
 
-
-buildTeam();
-
-inquirer
-  .prompt([
-    { // Manager Name
-      type: 'input',
-      message: 'What is the name of your Team Manager?',
-      name: 'manager-name',
-    },
-    { // Manager ID
-      type: 'input',
-      message: 'What is the employee if of your Team Manager?',
-      name: 'managaer-id',
-    },
-    { // Manager Email
-      type: 'input',
-      message: 'What is the email address of your Team Manager?',
-      name: 'manager-email',
-    },
-    { // Manager Office Number
-        type: 'input',
-        message: 'What is the office number of your Team Manager?',
-        name: 'manager-office'
-    },
-    { // Team Continue/End
-        type: 'list',
-        message: 'Would you like to add memebers to your team or are you finished building?',
-        name: 'team-builder',
-        choices: ['Engineer','Intern','Finished Building']
-    },
-    {// Engineer Name
-        type: 'input',
-        name: 'engineer-name',
-        message: 'What is the name of this Engineer?'
-    },
-    {// Engineer ID
-        type: 'input',
-        name: 'engineer-id',
-        message: 'What is the ID of this Engineer?'
-    },
-    {// Engineer Email
-        type: 'input',
-        name: 'engineer-email',
-        message: 'What is the email of this Engineer?'
-    },
-    {// Engineer GitHub 
-        type: 'input',
-        name: 'engineer-github',
-        message: 'What is the GitHub username of this Engineer?'
-    },
-    {// Intern Name
-        type: 'input',
-        name: 'intern-name',
-        message: 'What is the name of this Intern?'
-    },
-    {// Intern ID
-        type: 'input',
-        name: 'intern-id',
-        message: 'What is the id of this Intern?'
-    },
-    {// Intern Email
-        type: 'input',
-        name: 'intern-email',
-        message: 'What is the email of this Intern?'
-    },
-    {// Intern School
-        type: 'input',
-        name: 'intern-school',
-        message: 'What school does this Intern attend?'
-    }
-      
-
-  ]).then((data) => {
-    fs.writeFileSync('index.html', generateHTML(data))
-  })
-  .then(() => console.log('Successfully wrote to index.html'))
-  .catch((err) => console.log(err));
-
-  const generateHTML = (data) =>  {
-    return  `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-      <title>Document</title>
-    </head>
-    <body>
-      <div class="jumbotron jumbotron-fluid">
-      <div class="container">
-        <h1 class="display-4">Hi! My name is ${data.name}</h1>
-        <p class="lead">I am from ${data.location}.</p>
-        <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-        <ul class="list-group">
-          <li class="list-group-item">My GitHub URL is ${data.github} <a href="${data.github}">${data.github}</a></li>
-          <li class="list-group-item">LinkedIn: <a href="${data.linkedin}">${data.linkedin}</a></li>
-        </ul>
-      </div>
-    </div>
-    </body>
-    </html>
-    `
-  }
- ;
-
-// do {PROMPTS}
-//while (${data.team-builder} !== 'Finished Building')
-
+createManager();
--- src/page-template.js
+++ src/page-template.js
@@ -1,26 +1,99 @@
-function renderManager(manager) {
-    return `manager`;
-
-}
-
-function renderEngineers(engineer) {
-    return `engineer`;
-}
-
-function renderInterns(intern) {
-    return `intern`;
-}
-
-function generateTeam(teamMembers) {
+function makeManager(manager) {
     return `
-    ${renderManager(teamMembers.manager)}
-    ${renderManager(teamMembers.engineers)}
-    ${renderManager(teamMembers.interns)}
-    `
-}
-
-function render(teamMembers) {
-    return "this wil be HTML";
-}
-
-module.exports = render;
+    <div class="card employee-card">
+    <div class="card-header text-white bg-primary">
+        <h2 class="card-title">${manager.getName()}</h2>
+        <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${manager.getID()}</li>
+            <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+            <li class="list-group-item">Office number: ${manager.getOffice()}</li>
+        </ul>
+    </div>
+  </div>
+    `;
+  }
+  function makeEngineers(engineers) {
+    const markup = engineers.map((engineer) => {
+      return `<div class="card employee-card">
+        <div class="card-header text-white bg-primary">
+            <h2 class="card-title">${engineer.getName()}</h2>
+            <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${engineer.getID()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+                <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGitHub()}" target="_blank" rel="noopener noreferrer">${engineer.getGitHub()}</a></li>
+            </ul>
+        </div>
+    </div>
+        `;
+    });
+    return markup.join(",");
+  }
+  function makeInterns(interns) {
+    const markup = interns.map((intern) => {
+      return `
+          <div class="card employee-card">
+      <div class="card-header text-white bg-primary">
+          <h2 class="card-title">${intern.getName()}</h2>
+          <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
+      </div>
+      <div class="card-body">
+          <ul class="list-group">
+              <li class="list-group-item">ID: ${intern.getID()}</li>
+              <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+              <li class="list-group-item">School: ${intern.getSchool()}</li>
+          </ul>
+      </div>
+  </div>`;
+    });
+    return markup.join(",");
+  }
+  
+  function generateTeam(teamMembers) {
+    return `
+          ${makeManager(teamMembers.manager)}
+          ${makeEngineers(teamMembers.engineers)}
+          ${makeInterns(teamMembers.interns)}
+      `;
+  }
+  
+  function render(teamMembers) {
+    return `
+    <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>My Team</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/c502137733.js"></script>
+  </head>
+  <body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 jumbotron mb-3 team-heading bg-danger">
+                <h1 class="text-center text-white">My Team</h1>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="team-area col-12 d-flex justify-content-center">
+                ${generateTeam(teamMembers)}
+            </div>
+        </div>
+    </div>
+  </body>
+  </html>
+    `;
+  }
+  
+  module.exports = render;
